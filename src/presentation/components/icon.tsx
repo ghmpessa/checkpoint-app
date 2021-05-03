cef63ced9a1ac9673e85d0a6efd4a48498e479db
Gustavo Pessa <ghmpessa@gmail.com>
2021-05-02 22:17:17 -0300

    feat: add icons component

---
+++ src/presentation/components/icon.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+export enum IconName {
+  group = 'account-multiple',
+  home = 'home',
+  list = 'format-list-bulleted',
+  share = 'share-variant',
+  comment = 'message-reply',
+  account = 'account-circle',
+  person = 'account',
+  exit = 'exit-to-app',
+  back = 'arrow-left'
+}
+
+type Props = {
+  name: IconName
+  size?: number
+  color?: string
+}
+
+const Icon: React.FC<Props> = ({ name, size = 30, color = '#666666', ...props }: Props) => {
+  return (
+    <MaterialCommunityIcons
+      size={size}
+      name={name}
+      color={color}
+    />
+  )
+}
+
+export default Icon
