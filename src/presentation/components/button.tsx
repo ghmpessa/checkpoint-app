c490fc4e52213f86fe4b3f18a2ab29050e2998a8
Gustavo Pessa <ghmpessa@gmail.com>
2021-05-02 22:15:07 -0300

    feat: add button component

---
+++ src/presentation/components/button.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
+
+interface Props extends RectButtonProps {
+  title: string
+  text?: boolean
+  fontSize?: number
+}
+
+const Button: React.FC<Props> = ({ title, text = false, fontSize = 20, ...rest }: Props) => {
+  return (
+    <RectButton
+      style={text ? styles.textButton : styles.container}
+      {...rest}
+    >
+      <Text style={[styles.text, text && styles.textColor, { fontSize: fontSize }]}>{title}</Text>
+    </RectButton>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 32,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#40A900',
+    marginHorizontal: 10,
+    paddingHorizontal: 10
+  },
+  text: {
+    color: 'white',
+    fontFamily: 'Montserrat-Bold'
+  },
+  textColor: {
+    color: '#40A900'
+  },
+  textButton: {
+    fontSize: 16,
+    fontFamily: 'Montserrat-Bold',
+    backgroundColor: 'transparent',
+    marginHorizontal: 10
+  }
+})
+
+export default Button
