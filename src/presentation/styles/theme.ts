0d21e6b6806f0508037930b4f26909e6ff455ea2
Gustavo Pessa <ghmpessa@gmail.com>
2021-05-02 22:14:33 -0300

    feat: add project theme

---
+++ src/presentation/styles/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#40A900',
+    accent: '#71b9ad',
+    background: '#121212',
+    text: '#ffffff',
+    placeholder: '#f6f6f6'
+  }
+}
