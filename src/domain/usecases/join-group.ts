126498eda2bd71731530c1b8180b680d104cd0bc
Gustavo Pessa <ghmpessa@gmail.com>
2021-06-13 23:41:21 -0300

    feat: add JoinGroup interface

---
+++ src/domain/usecases/join-group.ts
@@ -0,0 +1,10 @@
+import { GroupModel } from '../models'
+
+export type JoinParams = {
+  groupId: string
+  bind: boolean
+}
+
+export interface JoinGroup {
+  join: (params: JoinParams) => Promise<GroupModel>
+}
